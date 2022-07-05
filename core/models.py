0e25a89633a3d28ee6bd455ca7afcea410e5500b
Daniele da Silva <silva.deniele.ti@gmail.com>
2022-07-05 08:48:43 -0300

    commit Inicial

---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.descricao
+        
+# return self.descricao.upper()
