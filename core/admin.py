5b0619d2232a9e01537be0250fe9f4b812759023
Daniele da Silva <silva.deniele.ti@gmail.com>
2022-07-07 16:50:31 -0300

    commit classe Livro

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from core.models import Categoria
+from core.models import Autor, Categoria, Editora
 
+admin.site.register(Autor)
 admin.site.register(Categoria)
+admin.site.register(Editora)
--- core/models.py
+++ core/models.py
@@ -9,3 +9,35 @@ class Categoria(models.Model):
 
 
 # return self.descricao.upper()
+
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name_plural = "Autores"
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros"
+    )
