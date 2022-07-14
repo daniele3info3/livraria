c11c61a7087fbf1a0a07b03cf3523ffe8c940129
Daniele da Silva <silva.deniele.ti@gmail.com>
2022-07-14 17:00:53 -0300

    drf editora e livro

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework.serializers import ModelSerializer
-from core.models import Categoria
+from core.models import Categoria, Editora, Livro
 
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
+        fields = "__all__"
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = "__all__"
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
         fields = "__all__"
--- livraria/urls.py
+++ livraria/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
-from core.views import CategoriaViewSet
+from core.views import CategoriaViewSet, EditoraViewSet, LivroViewSet
 
 router = DefaultRouter()
 router.register('categorias', CategoriaViewSet)
+router.register('editoras', EditoraViewSet)
+router.register('livros', LivroViewSet)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
